8cd79de387601bb93f505f6dd6bd68231ce0aae1
Dominic <mrdjfraser@gmail.com>
2017-03-29 15:11:38 +0100

    added houses table with foreign key to students

--- db/hogwarts_db.sql
+++ db/hogwarts_db.sql
@@ -1,3 +1,4 @@
+DROP TABLE houses;
 DROP TABLE students;
 
 
@@ -5,10 +6,12 @@ CREATE TABLE students (
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  house VARCHAR(255),
-  age INT4
+  house_id INT4 REFERENCES houses(id),
+  age INT4 CHECK (age > 0)
 );
 
-
-
+CREATE TABLE houses (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
 
