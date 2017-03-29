73d50f28859eedc82aa391f9b1593391e571357d
Dominic <mrdjfraser@gmail.com>
2017-03-29 16:45:23 +0100

    fixing syntax errors relating to house name being house id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,29 @@ require('pry')
 house1 = House.new({
   'house_name' => 'house1'
   })
+house2 = House.new({
+  'house_name' => 'house2'
+  })
 
 house1.save
+house2.save
 
-# student1 = Student.new({
-#   'first_name' => '1fn',
-#   'last_name' => '1ln',
-#   'house_name' => 'house1',
-#   'age' => 15
-#   })
+student1 = Student.new({
+  'first_name' => '1fn',
+  'last_name' => '1ln',
+  'house_id' => house1.id,
+  'age' => 15
+  })
 
-# student2 = Student.new({
-#   'first_name' => '2fn',
-#   'last_name' => '2ln',
-#   'house_name' => 'house2',
-#   'age' => 16
-#   })
+student2 = Student.new({
+  'first_name' => '2fn',
+  'last_name' => '2ln',
+  'house_id' => house2.id,
+  'age' => 16
+  })
 
-# student1.save
-# student2.save
+student1.save
+student2.save
 
 
 
--- models/student.rb
+++ models/student.rb
@@ -8,14 +8,14 @@ class Student
     @id = student_hash['id'].to_i
     @first_name = student_hash['first_name']
     @last_name = student_hash['last_name']
-    @house = student_hash['house']
+    @house_id = student_hash['house_id'].to_i
     @age = student_hash['age'].to_i
   end
 
 ### Instance methods
 
   def save()
-    sql = "INSERT INTO students (first_name, last_name, house_name, age) VALUES ('#{@first_name}','#{@last_name}','#{@house}',#{@age}) RETURNING id"
+    sql = "INSERT INTO students (first_name, last_name, house_id, age) VALUES ('#{@first_name}','#{@last_name}',#{@house_id},#{@age}) RETURNING id"
 
     student_array = SqlRunner.run(sql)
     @id = student_array.first['id'].to_i
