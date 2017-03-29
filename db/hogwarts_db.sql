84252d748785a28510a6ea06173793f911c0bf50
Dominic <mrdjfraser@gmail.com>
2017-03-29 15:20:03 +0100

    changed which tables create first

--- db/hogwarts_db.sql
+++ db/hogwarts_db.sql
@@ -1,17 +1,15 @@
-DROP TABLE houses;
 DROP TABLE students;
-
-
-CREATE TABLE students (
-  id SERIAL4 PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  house_id INT4 REFERENCES houses(id),
-  age INT4 CHECK (age > 0)
-);
+DROP TABLE houses;
 
 CREATE TABLE houses (
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
 );
 
+CREATE TABLE students (
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  house_id INT4 REFERENCES houses(id) ON DELETE CASCADE,
+  age INT4 CHECK (age > 11)
+);
