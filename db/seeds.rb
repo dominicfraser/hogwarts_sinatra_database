3c015a5f656283ab03e31f0aa4d63d2d4fd21d3b
Dominic <mrdjfraser@gmail.com>
2017-03-29 12:10:19 +0100

    student seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
 require_relative('')
 
-student1 = 
+student1 = Student.new({
+  'first_name' => 'student1fn',
+  'last_name' => 'student1ln',
+  'house' => 'house1',
+  'age' => 15
+  })
+
+student2 = Student.new({
+  'first_name' => 'student2fn',
+  'last_name' => 'student2ln',
+  'house' => 'house2',
+  'age' => 16
+  })
